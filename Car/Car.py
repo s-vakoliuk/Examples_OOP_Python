90e003d0ab103bd8f4bc65455b2798b994d55cd6
serhii <s.vakoliuk@gmail.com>
2022-07-25 22:46:43 +0300

    Add odometer

--- Car/Car.py
+++ Car/Car.py
@@ -1,16 +1,24 @@
 class Car:
     """Модель класу Car (автомобіль)"""
+
     def __init__(self, make, model, year):
         """Ініціалізація атрибутів класу Car"""
         self.make = make
         self.model = model
         self.year = year
+        self.odometer_reading = 0
 
     def get_descriptive_name(self):
         """Getter повертає інформацію про авто"""
-        long_name=f"{self.make} - {self.model} - {self.year}"
+        long_name = f"{self.make} - {self.model} - {self.year}"
         return long_name
 
+    def read_odometer(self):
+        """Вивести повідомлення пробігу автомобіля"""
+        print(f" Цей автомобіль має {self.odometer_reading} км. пробігу")
 
-my_car=Car("FIAT", "Grande Punto", "2007")
+
+my_car = Car("FIAT", "Grande Punto", "2007")
 print(my_car.get_descriptive_name())
+
+my_car.read_odometer()
