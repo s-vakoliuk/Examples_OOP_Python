8526880fb2e3062bcd3bd01e5c3e070357a23503
serhii <s.vakoliuk@gmail.com>
2022-07-25 23:12:05 +0300

    Update odometer

--- Car/Car.py
+++ Car/Car.py
@@ -17,10 +17,22 @@ class Car:
         """Вивести повідомлення пробігу автомобіля"""
         print(f"Цей автомобіль має {self.odometer_reading} км. пробігу")
 
+    def update_odometer(self, kmage):
+        """Задати/Оновити значення одометра"""
+        if kmage >= self.odometer_reading:
+            self.odometer_reading = kmage
+        else:
+            print("Ти не можеш відмотати показник одометра назад")
+
+
+
 
 my_car = Car("FIAT", "Grande Punto", "2007")
 print(my_car.get_descriptive_name())
 
-#Внесення даних одометра
-my_car.odometer_reading = 192000
+# Внесення даних одометра
+my_car.update_odometer(193000)
+# Зчитування даних одометра
 my_car.read_odometer()
+
+
